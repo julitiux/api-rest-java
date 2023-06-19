bbebcd9b9dc5de7c31fe7848d80770fce1c24d15
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-19 17:06:50 -0600

    binding by param

--- src/main/java/com/apirest/controllers/BindingDataController.java
+++ src/main/java/com/apirest/controllers/BindingDataController.java
@@ -1,4 +1,16 @@
 package com.apirest.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BindingDataController {
+
+  @RequestMapping(value = "/param")
+  public @ResponseBody String byParam(@RequestParam String foo) {
+    return "binding by param " + foo;
+  }
+
 }
