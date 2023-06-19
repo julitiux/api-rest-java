01e9cd26c344ca07a08b2f132caa09602025c3f7
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-18 19:28:29 -0600

    mapping by path

--- src/main/java/com/apirest/controllers/RequestMapController.java
+++ src/main/java/com/apirest/controllers/RequestMapController.java
@@ -1,7 +1,15 @@
 package com.apirest.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RequestMapController {
+
+  @RequestMapping("/simpleMapping/path")
+  public @ResponseBody String byPath() {
+    return "mapping by path";
+  }
+
 }
