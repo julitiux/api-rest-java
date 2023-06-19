c2af4f4b463e1eb55093ef8e50052f9559cb0803
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-19 17:29:46 -0600

    binding by path variable

--- src/main/java/com/apirest/controllers/BindingDataController.java
+++ src/main/java/com/apirest/controllers/BindingDataController.java
@@ -2,6 +2,7 @@ package com.apirest.controllers;
 
 import com.apirest.domains.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,5 +19,11 @@ public class BindingDataController {
   public @ResponseBody String byBean(User user) {
     return "binding by bean " + user;
   }
+
+  @RequestMapping(value = "path/{var}")
+  public @ResponseBody String byPathVariable(@PathVariable String var) {
+    return "binding by path variable " + var;
+  }
+
 
 }
