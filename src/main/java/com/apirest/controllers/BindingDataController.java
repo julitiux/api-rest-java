e8a5f7b21e3861531f8e55c370c143bf490d869b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-19 17:10:05 -0600

    binding by bean

--- src/main/java/com/apirest/controllers/BindingDataController.java
+++ src/main/java/com/apirest/controllers/BindingDataController.java
@@ -1,5 +1,6 @@
 package com.apirest.controllers;
 
+import com.apirest.domains.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BindingDataController {
 
-  @RequestMapping(value = "/param")
+  @RequestMapping(value = "/byParam")
   public @ResponseBody String byParam(@RequestParam String foo) {
     return "binding by param " + foo;
+  }
+
+  @RequestMapping(value = "/byGroup")
+  public @ResponseBody String byBean(User user) {
+    return "binding by bean";
   }
 
 }
