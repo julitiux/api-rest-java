df2971485d44c59133f60aabc1bbd1faf3c04b03
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-06 00:44:36 -0600

    implement method handle, get HttpEntity just for get body and headers and reponse with HttpHeaders using ResponseEntity with a HttpStatus.CREATED

--- src/main/java/com/apirest/controllers/HttpEntityController.java
+++ src/main/java/com/apirest/controllers/HttpEntityController.java
@@ -1,8 +1,25 @@
 package com.apirest.controllers;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.UnsupportedEncodingException;
 
 @Controller
 public class HttpEntityController {
+
+  @RequestMapping("/something")
+  public ResponseEntity<String> handle(HttpEntity<byte[]> requestEntity) throws UnsupportedEncodingException {
+    String requestHeader = requestEntity.getHeaders().getFirst("MyRequestHeader");
+    byte[] requestBody = requestEntity.getBody();
+
+    HttpHeaders responseHeaders = new HttpHeaders();
+    responseHeaders.set("MyResponseHeader", "MyValue");
+    return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+  }
 
 }
