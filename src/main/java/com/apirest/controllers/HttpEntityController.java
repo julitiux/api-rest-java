d6a5a7160af1e62ed0b46b490aa66e020cfe1c94
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-06 00:58:33 -0600

    implement method responseEntityStatusCode() using ResponseEntity<String> customing status code 403

--- src/main/java/com/apirest/controllers/HttpEntityController.java
+++ src/main/java/com/apirest/controllers/HttpEntityController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.UnsupportedEncodingException;
 
@@ -20,6 +21,12 @@ public class HttpEntityController {
     HttpHeaders responseHeaders = new HttpHeaders();
     responseHeaders.set("MyResponseHeader", "MyValue");
     return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+  }
+
+  @RequestMapping(value = "/response/entity/status", method = RequestMethod.GET)
+  public ResponseEntity<String> responseEntityStatusCode() {
+    String string = "The ResponseBody String with custom status code";
+    return new ResponseEntity<>(string, HttpStatus.FORBIDDEN);
   }
 
 }
